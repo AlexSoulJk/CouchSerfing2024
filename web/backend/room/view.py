d3debb104d098be36cef72ca6f3da2a092ed8152
AlexSoulJk <alex.solomatow2016@yandex.ru>
2024-04-17 22:53:13 +0300

    Change create room end-point

--- web/backend/room/view.py
+++ web/backend/room/view.py
@@ -1,18 +1,23 @@
 from fastapi import APIRouter
 from . import crud
-from .schemas import Room, RoomCreate
+from .schemas import Room, RoomCreate, RoomUpdateParticle
 from ..rule.schemas import RuleCreate
-from ..picture.schemas import PictureCreate
+from ..picture.schemas import PictureCreate, Picture
 
 router = APIRouter(tags=["Rooms"])
 
 
 @router.post("/{user_id}/", response_model=Room)
 async def create_room(user_id: int,
-                      room: RoomCreate,
-                      rules: list[RuleCreate],
-                      pictures: list[PictureCreate]):
-    return await crud.create_room(room_in=room, user_id=user_id,
-                                  pictures=pictures, rules=rules)
+                      room: RoomCreate):
+    return await crud.create_room(room_in=room, user_id=user_id)
 
 
+@router.patch("/{room_id}/", response_model=Room)
+async def update_particle(room_id: int,
+                          room_update: RoomUpdateParticle,
+                          rules_delete: list[RuleCreate],
+                          rules_create_list: list[RuleCreate],
+                          picture_delete_list: list[Picture],
+                          picture_create_list: list[PictureCreate]):
+    pass
